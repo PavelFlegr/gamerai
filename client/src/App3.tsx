3c634bb06892b730e7b9101509d080e22ea42cd9
pavel <pavelflegr@gmail.com>
2023-05-07 15:07:41 +0200

    chatbot ui improvements

--- client/src/App3.tsx
+++ client/src/App3.tsx
@@ -3,16 +3,14 @@ import {
   Button,
   Card,
   Container,
-  Divider,
-  MantineProvider,
+  Divider, Loader,
   ScrollArea,
   Space, Stack,
   Text,
   Textarea,
 } from '@mantine/core';
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { KeyboardEventHandler, useRef, useState } from 'react';
 import { useInputState, useTimeout } from '@mantine/hooks';
-import { io, Socket } from 'socket.io-client';
 import ky from 'ky';
 
 interface Response {
@@ -27,28 +25,39 @@ interface Message {
 export default function App3() {
   const [messages, setMessages] = useState<Message[]>([])
   const [input, setInput] = useInputState('')
+  const [responding, setResponding] = useState(false)
   const [cost, setCost] = useState(0);
   const viewport = useRef<HTMLDivElement>(null);
   const scrollToBottom = useTimeout(() => viewport.current?.scrollTo({ top: viewport.current.scrollHeight, behavior: 'smooth' }), 0);
 
+  const handleEnter: KeyboardEventHandler<HTMLTextAreaElement> = async (e) => {
+    if(e.key === 'Enter' && !e.getModifierState('Shift')) {
+      e.preventDefault()
+      await sendMessage()
+    }
+  }
+
   const sendMessage = async () => {
+    if(!input || responding) {
+      return
+    }
+    setResponding(true)
     let newMessages = [...messages, { author: 'user', text: input }]
     setMessages(newMessages)
     scrollToBottom.start()
-    if(input) {
-      setInput('')
-    }
+    setInput('')
     const response = await ky.post('/api/chat', {json: newMessages, timeout: false}).json<Response>()
     setCost(cost + response.cost)
     setMessages([...newMessages, response.message])
     scrollToBottom.start()
+    setResponding(false)
   }
 
   return (
       <Container mx="auto">
-        <Card shadow="sm">
-          <Stack justify="space-between">
-            <ScrollArea h={700} viewportRef={viewport}>
+        <Card h={'100vh'} shadow="sm">
+          <Stack h={'100%'} justify="space-between">
+            <ScrollArea viewportRef={viewport}>
               {messages.map((message, i) => (
                 <div key={i}>
                   <Text fz="xs">{message.author}</Text>
@@ -56,16 +65,20 @@ export default function App3() {
                   <Divider/>
                 </div>
               ))}
+              {responding ?
+                <div>
+                  <Text fz="xs">{'assistant'}</Text>
+                  <Loader variant='dots'/>
+                </div> : <></>}
             </ScrollArea>
             <Box>
               <Space h="xs"/>
-              <Textarea value={input} onChange={setInput}/>
+              <Textarea onKeyDown={handleEnter} minRows={5} value={input} onChange={setInput}/>
               <Space h="xs"/>
-              <Button onClick={sendMessage}>Send</Button>
+              <Button disabled={responding} onClick={sendMessage}>Send</Button>
               You spent { cost }$ in total to do this nonsense, good job
             </Box>
           </Stack>
-
         </Card>
       </Container>
   );
